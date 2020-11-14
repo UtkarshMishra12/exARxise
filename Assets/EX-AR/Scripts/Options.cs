94a8e0627f3f962c9b76499b4f0acf78686500f9
UtkarshMishra <nasautkarsh@gmail.com>
2020-11-14 12:24:34 +0530

    Final Commit

---
+++ Assets/EX-AR/Scripts/Options.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Options : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+
+    public void AR()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+
+}
